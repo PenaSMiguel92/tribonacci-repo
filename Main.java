f1e00ec5c17919723c101f04c6d31a1153a37755
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-03 22:14:28 -0700

    main method implemented

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-    
+        Solution sol = new Solution();
+
+        System.out.println(sol.tribonacci(4));
+        System.out.println(sol.tribonacci(25));
+        System.out.println(sol.tribonacci(100));
+
+        int n = 0;
+        while (n <= 37) {
+            System.out.println(sol.tribonacci(n));
+            n++;
+        }
     } 
 }
