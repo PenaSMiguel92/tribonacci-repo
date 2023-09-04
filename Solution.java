84dde5f6489c6ec3fe3c1c2d059081b5afca085e
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-03 22:09:39 -0700

    solution found

--- Solution.java
+++ Solution.java
@@ -1,4 +1,21 @@
+import java.util.Map;
+import java.util.HashMap;
+
 public class Solution {
-public int tribonacci(int n) {
-}
+    Map<Integer, Integer> memo = new HashMap<>();
+
+    public int tribonacci(int n) {
+        if (memo.containsKey(n)) {
+            return memo.get(n);
+        }
+        if (n == 0)
+            return 0;
+        if (n == 1 || n == 2)
+            return 1;
+        int val = tribonacci(n - 3) + tribonacci(n - 2) + tribonacci(n - 1);
+        if (!memo.containsKey(n)) {
+            memo.put(n, val);
+        }
+        return val;
+    }
 }
